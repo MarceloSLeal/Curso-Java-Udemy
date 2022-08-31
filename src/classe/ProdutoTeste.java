48f72ae0ffba4702c9f6e495d748a7c6917c9440
MarceloSLeal <marcelosleal@outlook.com>
2022-08-31 00:06:51 -0300

    Refatorando as estâncias dos objetos da classe Produto

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -17,8 +17,8 @@ public class ProdutoTeste {
         System.out.println(p1.nome);
         System.out.println(p2.nome);
 
-        double precoFinal1 = p1.preco * (1 - p1.desconto);
-        double precoFinal2 = p2.preco * (1 - p2.desconto);
+        double precoFinal1 = p1.precoComDesconto();
+        double precoFinal2 = p2.precoComDesconto(0.1);
         System.out.printf("Preço final produto 1: R$%.2f", precoFinal1);
         System.out.printf("\nPreço final produto 2: R$%.2f", precoFinal2);
 
