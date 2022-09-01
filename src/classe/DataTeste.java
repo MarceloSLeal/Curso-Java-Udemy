a67426a988d770ce4550a34b408aa80881194ccf
MarceloSLeal <marcelosleal@outlook.com>
2022-09-01 01:11:22 -0300

    Usando os construtores nas instâncias e refatorando o código

--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -6,18 +6,11 @@ public class DataTeste {
     public static void main(String[] args){
 
         Data dat1 = new Data();
-        Data dat2 = new Data();
-
-        dat1.dia = 10;
-        dat1.mes = 6;
-        dat1.ano = 2010;
-
-        dat2.dia = 22;
-        dat2.mes = 2;
-        dat2.ano = 2022;
+        Data dat2 = new Data(15, 10, 1980);
 
         System.out.println(dat1.obterDataFormatada());
         System.out.println(dat2.obterDataFormatada());
+
     }
 
 }
