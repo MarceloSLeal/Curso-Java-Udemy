f3bffec0813a5771d303131db5d1be451d27486e
MarceloSLeal <marcelosleal@outlook.com>
2022-09-07 13:46:47 -0300

    Refatorando para adicionar o This e referenciar as variáveis da instância.

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -11,12 +11,13 @@ public class Data {
         dia = 1;
         mes = 1;
         ano = 1970;
+//        this(1, 1, 1970); //opção de chamar outro construtor dentro de um constutor, só da pra fazer isso dentro de um construtor.
     }
 
-    Data (int diaInicial, int mesInicial, int anoInicial) {
-        dia = diaInicial;
-        mes = mesInicial;
-        ano = anoInicial;
+    Data (int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
 
     String obterDataFormatada() {
