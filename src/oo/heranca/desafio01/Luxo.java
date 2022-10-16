9c6aec34469bc882e6b2c92de89616ca87f41e1e
MarceloSLeal <marcelosleal@outlook.com>
2022-10-16 20:28:42 -0300

    Exemplo de como um método dentro de uma interface não necessariamente precisa ser implementado, com a definição "default" para o método, sem essa definição os métodos são altomaticamente abstratos, ou seja, precisam ser implementados nas classes.

--- src/oo/heranca/desafio01/Luxo.java
+++ src/oo/heranca/desafio01/Luxo.java
@@ -5,4 +5,10 @@ public interface Luxo {
     void ligarAr();
     void desligarAr();
 
+    //Exemplo de como um método dentro de uma interface não necessarimente precisa ser
+    //implementado
+    default int velocidadeDoAr() {
+        return 1;
+    }
+
 }
