26f9c9c73a268b3e12589ca820ebff8655d9a182
MarceloSLeal <marcelosleal@outlook.com>
2022-11-05 15:42:05 -0300

    Outros exemplos de funções Stream

--- src/streams/Aluno.java
+++ src/streams/Aluno.java
@@ -14,4 +14,24 @@ public class Aluno {
         return nome + " Tem nota " + nota;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Aluno aluno = (Aluno) o;
+
+        if (Double.compare(aluno.nota, nota) != 0) return false;
+        return nome.equals(aluno.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = nome.hashCode();
+        temp = Double.doubleToLongBits(nota);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
