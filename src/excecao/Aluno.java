d082b73c30deefe238c4322cb661f28322c0e90f
MarceloSLeal <marcelosleal@outlook.com>
2022-11-08 23:26:17 -0300

    Exemplos de execções personalizadas

--- src/excecao/Aluno.java
+++ src/excecao/Aluno.java
@@ -2,8 +2,8 @@ package excecao;
 
 public class Aluno {
 
-    final String nome;
-    final double nota;
+    public String nome;
+    public double nota;
 
     public Aluno(String nome, double nota) {
         this.nome = nome;
