1161162c32e23c4d481503d0c31816bb2ada37b8
MarceloSLeal <marcelosleal@outlook.com>
2023-02-13 19:30:02 -0300

    Criando um banco no servidor por uma conexão JDBC no Java

--- src/main/java/jdbc/CriarBanco.java
+++ src/main/java/jdbc/CriarBanco.java
@@ -3,8 +3,9 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class TesteConexao {
+public class CriarBanco {
 
     public static void main(String[] args) throws SQLException {
 
@@ -14,7 +15,10 @@ public class TesteConexao {
 
         Connection conexao = DriverManager.getConnection(url, usuario, senha);
 
-        System.out.println("Conexão efetuada com sucesso");
+        Statement stmt = conexao.createStatement();
+        stmt.execute("CREATE DATABASE IF NOT EXISTS curso_java");
+
+        System.out.println("Banco criado com sucesso!");
         conexao.close();
 
     }
